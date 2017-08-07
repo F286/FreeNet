d554e2ff69819cf2a6fa34f5ee40b6a797619aa5
F286 <freedebreuil@gmail.com>
2017-08-07 16:22:14 +0900

    initial setup for FreeFlow

---
+++ Freeflow/Assets/Scripts/RunShader.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RunShader : MonoBehaviour {
+
+    public ComputeShader shader;
+    public MeshRenderer renderer;
+
+    public float[] filter;
+
+    public void Update()
+    {
+        int kernelHandle = shader.FindKernel("CSMain");
+
+        RenderTexture tex = new RenderTexture(256, 256, 24);
+        tex.enableRandomWrite = true;
+        tex.Create();
+
+        // Set filter buffer
+        ComputeBuffer filterBuffer = new ComputeBuffer(filter.Length, 4);
+        filterBuffer.SetData(filter);
+
+        shader.SetTexture(kernelHandle, "Result", tex);
+        shader.SetBuffer(kernelHandle, "Filter", filterBuffer);
+        shader.Dispatch(kernelHandle, 256 / 8, 256 / 8, 1);
+
+        // Get filter buffer
+        filterBuffer.GetData(filter);
+
+        renderer.material.mainTexture = tex;
+    }
+    
+}
